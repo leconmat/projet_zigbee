70cbd1a906873b3028e3e6e229278d88b8222871
belhajm44 <belhajmohammed37@gmail.com>
2023-05-26 11:47:33 +0200

    FIFO_RX VALID RTL

--- modules/fifo_rx/rtl/src/fifo_rx.sv
+++ modules/fifo_rx/rtl/src/fifo_rx.sv
@@ -1,72 +1,27 @@
-module fifo_rx #(
-	// Parameters
-	parameter WIDTH = 8,
-	parameter DEPTH = 64
-)				
+module fifo_rx				
+#(
+					parameter WIDTH = 8,
+					parameter DEPTH = 64
+)		
 (
-	clk, // horloge systeme 
-	reset_n,
-	cdr_en,
-
-	// APB signals
-	pwdata,
-	paddr,
-	psel,
-	pwrite,
-	penable,
-	pready,
-	pslverr,
-
-	// Output Tx
-	data_out, 
-	IQ_rate,
-	mem_state // 0 memoire vide, 1 memoire remplie ou partiellement remplie
+					input logic clk, // horloge systeme 
+					input logic reset_n,
+					input logic en_cdr,
+					// APB signals
+					input logic data_in,
+					input logic psel,
+					input logic pwrite,
+					input logic penable,
+					output logic pready,
+					output logic pslverr,
+					// Output Tx
+					output logic [WIDTH-1:0] prdata, 
+					output reg mem_state // 0 memoire vide, 1 memoire remplie ou partiellement remplie
 );
 
 
-
 //////////////////////////////////////////////
-// IO declarations
-//////////////////////////////////////////////
-input logic clk, // horloge systeme 
-input logic reset_n,
-
-
-// APB signals
-input logic pwdata,
-input logic [7:0] paddr,
-input logic psel,
-input logic pwrite,
-input logic penable,
-output logic pready,
-output logic pslverr,
-
-// Output Tx
-output logic [WIDTH-1:0] data_out, 
-// output logic IQ_rate,
-output reg mem_state // 0 memoire vide, 1 memoire remplie ou partiellement remplie
-
-reg i = 0;
-
-// Store one bit data into shift register
-always @(posedge clk, negedge reset_n) begin
-	if (!reset_n) begin
-			shift_regiter <= 8'b00000000;
-	end
-	else begin
-		if (i >= WIDTH) begin
-			i <= 0;
-		end
-		else begin
-			shift_regiter[i] <= pwdata;
-			i <= i + 1;
-	end
-end
-
-
-
-//////////////////////////////////////////////
-// Internal signals
+//////////// Internal signals
 //////////////////////////////////////////////
 // Memory signals 
 parameter PTR_WIDTH = $clog2(DEPTH);
@@ -76,17 +31,17 @@ logic [PTR_WIDTH:0] wr_ptr, rd_ptr;
 // Memory flag
 logic full;  
 logic empty; 
-// Rate change counters 50 MHz -> 2 MHz
-reg [4:0] counter_clock;
-reg [2:0] compteur;
 reg [7:0] shift_register;
-integer i;
-logic load;
+reg [WIDTH-1:0] i = 0;
+reg en_cdr_prec;
+assign pready = 1'b1;
 
-//////////////////////////////////////////////
-// BEGIN MEMORY LOGIC 
-//////////////////////////////////////////////
-// Pointers managing
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////// BEGIN MEMORY LOGIC ////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
 always_comb begin
 	if((wr_ptr[PTR_WIDTH-1:0]) == (rd_ptr[PTR_WIDTH-1:0])) begin
 		full = (wr_ptr[PTR_WIDTH]) ^ (rd_ptr[PTR_WIDTH]); // carry different => full 
@@ -100,7 +55,7 @@ end
 
 // Memory state logic 
 always_comb begin 
-	if (empty == 1'b1) begin
+	if (full == 1'b1) begin
 		mem_state = 1'b0; // Memoire vide 
 	end
 	else
@@ -109,76 +64,95 @@ end
 
 // APB Error logic
 always_comb begin
-	if (full == 1) begin 
+	if (empty == 1) begin 
 		pslverr = 1'b1; // Memoire pleine 
 	end
 	else 
-		pslverr =1'b0;
+		pslverr = 1'b0;
 end
 
-//////////////////////////////////////////////
-// END MEMORY LOGIC 
-//////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////// END MEMORY LOGIC //////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 
 
+/////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////// BEGIN WRITE LOGIC /////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////
-// BEGIN WRITE LOGIC 
-//////////////////////////////////////////////
-assign pready = 1;
-assign paddr = 8'b00000000;
+assign wr_en = !en_cdr_prec && en_cdr && !full ;
 
-always_ff @(posedge clk) begin
-	if(!en_cdr)
-		mem[wr_ptr] <= shift_register;
-	else
-		mem[wr_ptr] <= mem[wr_ptr];
-end	
-
-// Write pointer logic
-always_ff @(posedge clk, negedge reset_n) begin
-	if(!reset_n) begin
-		wr_ptr <= 'h0;
+// Shift register SERIAL IN PARALLEL OUT 
+always @(posedge clk, negedge reset_n) begin
+	if (!reset_n) begin
+		shift_register <= 8'b00000000;
 	end
 	else begin
-		if(!en_cdr)
-			wr_ptr <= wr_ptr + 1;
-		else
-			wr_ptr <= wr_ptr;
+		if (i >= WIDTH)
+			i <= 0;
+		else begin
+			shift_register[i] <= data_in;
+			i <= i + 1;
+		end
 	end
 end
-//////////////////////////////////////////////
-// END WRITE LOGIC 
-//////////////////////////////////////////////
+
+always_ff @(posedge clk, negedge reset_n) begin
+	if(~reset_n) begin
+		for(integer i = 0; i < DEPTH; i++) begin
+			mem[i] <= 0;
+		end
+		wr_ptr <= 0;
+	end
+	else begin
+		en_cdr_prec <= en_cdr;	
+		if(!en_cdr_prec && en_cdr && !full) begin
+			mem[wr_ptr[PTR_WIDTH-1:0]] <= shift_register;
+			wr_ptr <= wr_ptr + 1;
+		end
+		else begin
+			mem[wr_ptr[PTR_WIDTH-1:0]] <= mem[wr_ptr[PTR_WIDTH-1:0]];
+			
+		end
+	end
+end
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// END WRITE LOGIC ////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 
 
+/////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////// BEGIN READ LOGIC //////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////
-// BEGIN READ LOGIC
-//////////////////////////////////////////////
 assign rd_en = psel && penable && !pwrite && !empty;
 
 always_ff @(posedge clk) begin
 	if(rd_en)
-		data_out[rd_ptr] <= mem[rd_ptr];
+		prdata <= mem[rd_ptr[PTR_WIDTH-1:0]];
 	else
-		data_out[rd_ptr] <= data_out[rd_ptr];
+		prdata <= 'h0;
 end	
 
 always_ff @(posedge clk, negedge reset_n) begin
-	if(!reset_n) begin
+	if(~reset_n) begin
 		rd_ptr <= 'h0;
 	end
 	else begin
-		if (rd_en) begin
+		if (rd_en) 
 			rd_ptr <= rd_ptr + 1;
-		end
-		else begin
+		else
 			rd_ptr <= rd_ptr;
-		end
 	end
 end
-//////////////////////////////////////////////
-// END READ LOGIC
-//////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////// END READ LOGIC ///////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+
+
 endmodule
+
+
+
---
+++ modules/fifo_rx/testbench/src/fifo_rx_tb.sv
@@ -0,0 +1,59 @@
+`timescale 1ns/1ps
+
+module fifo_rx_tb ();
+
+//System Inputs
+logic clk, reset_n, en_cdr;
+
+// APB signals
+logic data_in, psel, pwrite, penable, pready, pslverr,a;
+integer y = 0 ;
+integer j = 0 ;
+// Output Tx
+logic [7:0] prdata;
+reg mem_state; // 0 memoire vide, 1 memoire remplie ou partiellement remplie
+
+fifo_rx DUT(.clk(clk),.reset_n(reset_n), .en_cdr(en_cdr), 
+	    .data_in(data_in), .psel(psel), .pwrite(pwrite), .penable(penable), .pready(pready), .pslverr(pslverr),
+	    .prdata(prdata), .mem_state(mem_state));
+
+	//reset
+	initial begin
+		reset_n = 0;
+		#20; 
+		reset_n = 1;
+		#20; 
+	end
+
+	// Write bench
+	initial begin
+		data_in = 0;
+		clk = 0;
+		forever begin
+			clk = 0;
+			#10;
+			clk = 1;
+			#10;
+			data_in = $urandom;
+		end
+	end
+	initial begin
+		en_cdr = 0;
+		forever begin
+			#250;
+			en_cdr = ~en_cdr;	
+		end
+	end
+
+	//Test Lecture
+	initial begin
+		psel = 1'b0;
+		pwrite = 1'b0;
+		penable = 1'b0;
+		#500;
+	 	psel = 1'b1;
+		pwrite = 1'b0;
+		penable = 1'b1;
+	end 
+
+endmodule
